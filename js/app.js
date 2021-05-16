8a27e2692059fa14896d6bbe551774594af45bfb
Shashank <shashank73744@gmail.com>
2021-05-15 17:20:46 -0700

    Bug fix

--- js/app.js
+++ js/app.js
@@ -345,9 +345,9 @@ App = {
 
   initWeb3: function() {
   	if (window.ethereum) {    
-  		window.ethereum.send('eth_requestAccounts').then((App)=>{
+  		window.ethereum.send('eth_requestAccounts').then(()=>{
   			window.web3 = new Web3(window.ethereum); 
-  			return App.initContract();
+  			return this.App.initContract();
   		});   
   		   
   	} else {
